de61bfe019ee855e3bd8414fab480ee8610c8982
Bidhan <bidhanbaniya789@gmail.com>
2023-05-12 09:39:32 -0700

    Cms detail store and validation test written

--- app/Http/Controllers/CmsDetailController.php
+++ app/Http/Controllers/CmsDetailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\cms;
 use App\Models\cms_detail;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CmsDetailController extends Controller
 {
@@ -13,5 +14,12 @@ class CmsDetailController extends Controller
         return response()->json(cms_detail::query()
             ->where('cms_id', $cm->id)
             ->get());
+    }
+
+    public function store(Request $request, cms $cm)
+    {
+        $request->validate(['name' => 'required']);
+
+        return response()->json($cm->cmsDetails()->create($request->all()), Response::HTTP_CREATED);
     }
 }
--- tests/Feature/CmsDetailTest.php
+++ tests/Feature/CmsDetailTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\cms;
+use App\Models\cms_detail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -21,5 +23,29 @@ class CmsDetailTest extends TestCase
 
         $this->assertEquals($cms_detail->name, $response[0]['name']);
         $this->assertEquals($cms->id, $response[0]['cms_id']);
+    }
+
+    public function test_cms_detail_store(): void
+    {
+        $cms = $this->createCMS();
+        $cms_detail = cms_detail::factory()->make();
+
+        $this->postJson(route('cms.cms_detail.store', $cms), [
+            'name' => $cms_detail->name,
+            'description' => $cms_detail->description
+        ])->assertCreated();
+
+        $this->assertDatabaseHas('cms_details', ['name' => $cms_detail->name]);
+    }
+
+    public function test_cms_detail_store_validation(): void
+    {
+        $this->withExceptionHandling();
+
+        $cms = $this->createCMS();
+
+        $this->postJson(route('cms.cms_detail.store', $cms))
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['name']);
     }
 }
