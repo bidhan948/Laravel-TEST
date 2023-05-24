4d6d545e93818ac7b222283d805e99411308469b
Bidhan <bidhanbaniya789@gmail.com>
2023-05-23 22:19:31 -0700

    It worked

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,58 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Google\Client;
 
 Route::get('/', function () { 
     return view('welcome');
+});
+
+Route::get('/drive', function () {
+    $client = new Client();
+    $client->setClientId('521002978912-jf9qp5lcifj34gdiutdpshffq42qtgkg.apps.googleusercontent.com');
+    $client->setClientSecret('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');
+    $client->setRedirectUri('http://localhost:8000/google-drive/callback');
+    $client->setScopes([
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+    ]);
+    $url = $client->createAuthUrl();
+    return redirect($url);
+});
+
+Route::get('/google-drive/callback', function () {
+    $client = new Client();
+    $client->setClientId('521002978912-jf9qp5lcifj34gdiutdpshffq42qtgkg.apps.googleusercontent.com');
+    $client->setClientSecret('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');
+    $client->setRedirectUri('http://localhost:8000/google-drive/callback');
+    $code = request('code');
+    $access_token = $client->fetchAccessTokenWithAuthCode($code);
+    return $access_token;
+});
+
+Route::get('upload', function () {
+    $client = new Client();
+    $access_token = 'ya29.a0AWY7CknykBRYBXfNd6Agb5Kham4AsOqYpeUf0tTcmRNA-INma676TPxkLMY55g7wtIn1jfMWzJPZTWV1KEdZ2Mj73eJjm9txOctKVl1cMebOmi-gpxlaErw1jiswEypQrw-z_gVEjaivimcW2LiR29LcSCEOaCgYKARQSARESFQG1tDrp5I7B-HI-o4Og1DWqCnB5xQ0163';
+
+    $client->setAccessToken($access_token);
+    $service = new Google\Service\Drive($client);
+    $file = new Google\Service\Drive\DriveFile();
+
+    DEFINE("TESTFILE", 'testfile-small.txt');
+    if (!file_exists(TESTFILE)) {
+        $fh = fopen(TESTFILE, 'w');
+        fseek($fh, 1024 * 1024);
+        fwrite($fh, "!", 1);
+        fclose($fh);
+    }
+
+    $file->setName("Hello World!");
+    $service->files->create(
+        $file,
+        array(
+            'data' => file_get_contents(TESTFILE),
+            'mimeType' => 'application/octet-stream',
+            'uploadType' => 'multipart'
+        )
+    );
 });
